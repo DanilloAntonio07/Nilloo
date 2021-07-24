c05c74f191da790fa09e64cc97004c892cb71c71
Danillo Antonio <danilloammartins123@gmail.com>
2021-07-24 11:40:23 -0300

    nilloo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hello!</Text>
+      <Text>Olá!</Text>
+      <Image style={styles.dzn} source={require("./assets/dzn.png")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +19,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    color: '#000',
+    fontSize: 40,
+  },
+
+  dzn: {
+    width: 250,
+    height: 250,
   },
 });
